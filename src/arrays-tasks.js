228bb310f44932b1c9bfc48be99786aa40e9a852
Denis <denisbernovich@gmail.com>
2024-01-07 21:43:40 +0300

    feat: part of the task completed

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -290,10 +290,6 @@ const flattenArray = (nestedArray) => {
  */
 function selectMany(/* arr, childrenSelector */) {
   throw new Error('Not implemented');
-  // return arr.reduce(
-  //   (result, current) => result.concat(childrenSelector(current)),
-  //   []
-  // );
 }
 
 /**
@@ -567,18 +563,19 @@ const shiftArray = (/* arr, n */) => {
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
 function sortDigitNamesByNumericOrder(/* arr */) {
-  const nums = {
-    zero: 0,
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-  };
+  // const nums = {
+  //   zero: 0,
+  //   one: 1,
+  //   two: 2,
+  //   three: 3,
+  //   four: 4,
+  //   five: 5,
+  //   six: 6,
+  //   seven: 7,
+  //   eight: 8,
+  //   nine: 9,
+  // };
+  throw new Error('Not implemented');
 }
 
 /**
